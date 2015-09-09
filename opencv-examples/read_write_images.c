0d6305a500707a7e7f13f23acd77bdf9a4fbe534
subnr01 <subnr01@gmail.com>
2015-09-09 09:24:31 -0400

    keypress + opencv

---
+++ opencv-examples/read_write_images.c
@@ -0,0 +1,30 @@
+// Example showing how to read and write images
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv/cvaux.hpp>
+
+int main(int argc, char** argv)
+{
+    IplImage * pInpImg = 0;
+    
+    // Load an image from file - change this based on your image name
+    pInpImg = cvLoadImage("/Users/admin/subs.jpg", CV_LOAD_IMAGE_UNCHANGED);
+    if(!pInpImg)
+    {
+        fprintf(stderr, "failed to load input image\n");
+        return -1;
+    }
+    
+    // Write the image to a file with a different name,
+    // using a different image format -- .png instead of .jpg
+    if( !cvSaveImage("/Users/admin/subs.png", pInpImg) )
+    {
+        fprintf(stderr, "failed to write image file\n");
+    }
+    
+    // Remember to free image memory after using it!
+    cvReleaseImage(&pInpImg);
+    
+    return 0;
+}
+
