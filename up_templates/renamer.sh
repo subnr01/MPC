edb89717c83b36ab71e92f4501f4ba981c2c432f
subnr01 <subnr01@gmail.com>
2015-12-01 01:09:03 -0500

    mobile templates

---
+++ up_templates/renamer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+id=1
+i=0
+x=`ls *.png`
+	for a in $x ; do
+	 	# let curNum=$(10000 + (100 * id) + i)
+	 	# echo "Curnum is $curNum"
+	 	# echo "id is $id"
+	 	# echo "File being read is $a"
+	 	# echo "I before is $i" 	
+	 		
+	 	if ((i==20))
+	 		then
+	 			let id=id+1
+	 			let i=0
+	 			echo "i is $i "
+	 			echo "Incrementing id now"
+	 	 	fi	
+	 	 	
+	 	let i1=100*id 		 
+	 	 	let fileval=$((10000 + i1 + i))
+	 	 	echo "file: $a  renamed: up_$fileval.png"
+	 	 	mv $a "up_$fileval.png"
+
+	 	let i=i+1 
+
+	 	
+ 	
+done
